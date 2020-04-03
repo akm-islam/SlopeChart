927fd59009ee8fb135217967a784c41d30b4d479
AKM Islam <mamunjahir.com@gmail.com>
2020-04-02 20:14:44 -0400

    Updates

---
+++ precache-manifest.2f970cfdd2426e57c14dfe383aa82bed.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dedaf9c1cc6782cb3a5271400ceb82c2",
+    "url": "/transparency/index.html"
+  },
+  {
+    "revision": "5ebf72fec7181097c9ff",
+    "url": "/transparency/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "2c508e8a214be3332dbb",
+    "url": "/transparency/static/css/main.55110192.chunk.css"
+  },
+  {
+    "revision": "5ebf72fec7181097c9ff",
+    "url": "/transparency/static/js/2.f927b628.chunk.js"
+  },
+  {
+    "revision": "f6cd8cbfdee776ccf4fb30c006d78b6c",
+    "url": "/transparency/static/js/2.f927b628.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c508e8a214be3332dbb",
+    "url": "/transparency/static/js/main.ae484e2e.chunk.js"
+  },
+  {
+    "revision": "8c2652b915989452564d",
+    "url": "/transparency/static/js/runtime-main.555891c7.js"
+  },
+  {
+    "revision": "af43d75fdc1e83421f4c95f5b8f22aee",
+    "url": "/transparency/static/media/fiscal.af43d75f.csv"
+  }
+]);
